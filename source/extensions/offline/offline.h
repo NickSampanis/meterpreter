8c147202ac0e6c033c4680df634146156f73a586
smack0pc0d3 <smack0pc0d3@github.com>
2015-08-19 02:02:29 +0300

    ext_offline bare

---
+++ source/extensions/offline/offline.h
@@ -0,0 +1,12 @@
+/*!
+ * @file offline.h
+ * @brief Entry point and intialisation declrations for the offline extention.
+ */
+#ifndef _METERPRETER_SOURCE_EXTENSION_OFFLINE_OFFLINE_H
+#define _METERPRETER_SOURCE_EXTENSION_OFFLINE_OFFLINE_H
+
+#define TLV_TYPE_EXTENSION_OFFLINE	0
+
+// Custom TLVs go here
+
+#endif
